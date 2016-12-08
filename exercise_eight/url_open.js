cbbe12f1117e1fc48fe9b7d606db14ea64d16186
oluwasegun <hazizsegun@gmail.com>
2016-12-08 13:23:31 +0100

    prototype function

--- exercise_eight/url_open.js
+++ exercise_eight/url_open.js
@@ -1,65 +1,20 @@
 'use strict';
 
-var _createClass = function () { 
-  function defineProperties(target, props) { 
-    for (var i = 0; i < props.length; i++) { 
-      var descriptor = props[i]; 
-      descriptor.enumerable = descriptor.enumerable || false; 
-      descriptor.configurable = true; 
-      if ("value" in descriptor) descriptor.writable = true; 
-      Object.defineProperty(target, descriptor.key, descriptor); 
-    } 
-  } 
-  return 
-  function (Constructor, protoProps, staticProps) { 
-    if (protoProps) defineProperties(Constructor.prototype, protoProps); 
-    if (staticProps) defineProperties(Constructor, staticProps); 
-    return Constructor; 
-  }; 
-}();
-
-function _classCallCheck(instance, Constructor) { 
-  if (!(instance instanceof Constructor)) { 
-    throw new TypeError("Cannot call a class as a function"); 
-  } 
+function Url(){
+  this.url = '';
+  this.newWindowConfig = 'height=450,width=4000,top=10,left=10,status=no,menubar=no,location=no,scrollbars=no,resizable=no,toolbar=no';
+  this.open();
 }
 
-var URlOpen = function () {
-  function URlOpen(windowWidth, windowHeight) {
-    _classCallCheck(this, URlOpen);
+Url.prototype.open = function(){
+  this.acceptUrl();
+  window.open(this.url,"newWindow",this.newWindowConfig);
+};
 
-    this.windowWidth = windowWidth;
-    this.windowHeight = windowHeight;
-    this.windowFeatures = 'width=' + this.windowWidth + ',height=' + this.windowHeight + ',menubar=no,location=no,resizable=no,scrollbars=no,status=no';
-    this.urlPattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-    this.init();
+Url.prototype.acceptUrl = function(){
+  while (this.url == null || this.url.trim().length < 1) {
+    this.url = prompt("Enter Url", "");
   }
+};
 
-  _createClass(URlOpen, [{
-    key: 'init',
-    value: function init() {
-      this.popUp(this.windowFeatures);
-    }
-  }, {
-    key: 'popUp',
-    value: function popUp(windowFeatures) {
-      var url = prompt('Please Enter your URL?(http://www.example.com)', 'http://www.');
-      if (url !== null && url.trim() !== '') {
-        if (!this.urlPattern.test(url)) {
-          alert('Oops! Invalid URL. Please provide URL');
-        } else {
-          if (url.search('http://')) {
-            url = 'http://' + url;
-          }
-          window.open(url, '_blank', this.windowFeatures);
-        }
-      } else {
-        alert('URL not provided. Please provide URL!');
-      }
-    }
-  }]);
-
-  return URlOpen;
-}();
-
-new URlOpen(4000, 450);
+new Url();
--- exercise_nine/country_mover.js
+++ exercise_nine/country_mover.js
@@ -1,86 +1,28 @@
 'use strict';
 
-var _createClass = function () { 
-  function defineProperties(target, props) { 
-    for (var i = 0; i < props.length; i++) { 
-      var descriptor = props[i]; 
-      descriptor.enumerable = descriptor.enumerable || false; 
-      descriptor.configurable = true; 
-      if ("value" in descriptor) descriptor.writable = true; 
-      Object.defineProperty(target, descriptor.key, descriptor); 
-    } 
-  } 
-  return function (Constructor, protoProps, staticProps) { 
-    if (protoProps) defineProperties(Constructor.prototype, protoProps); 
-    if (staticProps) defineProperties(Constructor, staticProps); 
-    return Constructor; 
-  }; 
-}();
-
-function _classCallCheck(instance, Constructor) { 
-  if (!(instance instanceof Constructor)) { 
-    throw new TypeError("Cannot call a class as a function"); 
-  } 
+function Select(selectElemId, transferToElemId , buttonELemId){
+  var dom = document;
+  this.myBox = dom.getElementById(selectElemId);
+  this.transferToBox = dom.getElementById(transferToElemId);
+  this.myButton = dom.getElementById(buttonELemId);
+  this.init(selectElemId, transferToElemId , buttonELemId);
 }
 
-var CountryMover = function () {
-  function CountryMover(addButton, removeButton) {
-    _classCallCheck(this, CountryMover);
+Select.prototype.init = function(selectElemId, transferToElemId , buttonELemId) {
+  var _this = this;
+  this.myButton.addEventListener("click", function(){
+    _this.transfer();
+  } , false);
+};
 
-    this.addButton = addButton;
-    this.removeButton = removeButton;
-    this.init();
+Select.prototype.transfer = function() {
+  var selectedIndex = this.myBox.selectedIndex;
+  if (selectedIndex > -1) {
+    var option = this.myBox.options[selectedIndex];
+    this.myBox.remove(selectedIndex);
+    this.transferToBox.add(option);
   }
+};
 
-  _createClass(CountryMover, [{
-    key: 'init',
-    value: function init() {
-      this.leftCountry = document.getElementById('leftCountry');
-      this.rightCountry = document.getElementById('rightCountry');
-      this.addButton.addEventListener('click', function () {
-        if (!this.isBoxEmpty(leftCountry)) {
-          this.moveSelectedOptions(leftCountry, rightCountry);
-        }
-      }.bind(this));
-
-      this.removeButton.addEventListener('click', function () {
-        if (!this.isBoxEmpty(rightCountry)) {
-          this.moveSelectedOptions(rightCountry, leftCountry);
-        }
-      }.bind(this));
-    }
-  }, {
-    key: 'isOptionSelected',
-    value: function isOptionSelected(fromBox) {
-      if (fromBox.selectedIndex === -1) {
-        alert('Please make a selection from ' + fromBox.name);
-        return false;
-      }
-      return true;
-    }
-  }, {
-    key: 'isBoxEmpty',
-    value: function isBoxEmpty(fromBox) {
-      if (fromBox.options.length !== 0) {
-        this.isOptionSelected(fromBox);
-        return false;
-      }
-      alert('No option to select from at this time');
-      return true;
-    }
-  }, {
-    key: 'moveSelectedOptions',
-    value: function moveSelectedOptions(fromBox, toBox) {
-      var selectedOption = fromBox.selectedOptions;
-      for (var i = selectedOption.length - 1; i >= 0; i--) {
-        toBox.add(selectedOption[i]);
-      };
-    }
-  }]);
-
-  return CountryMover;
-}();
-
-var addButton = document.querySelector('button[id="buttonAdd"]');
-var removeButton = document.querySelector('button[id="buttonRemove"]');
-new CountryMover(addButton, removeButton);
+var leftBox = new Select('leftCountry','rightCountry','add-btn');
+var rightBox = new Select('rightCountry','leftCountry','rmv-btn');
