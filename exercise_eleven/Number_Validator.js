5a7afb23f9ffeda64e29fcbc03658ee8ccec2658
oluwasegun <hazizsegun@gmail.com>
2016-12-08 13:39:51 +0100

    using prototype

--- exercise_eleven/Number_Validator.js
+++ exercise_eleven/Number_Validator.js
@@ -1,110 +1,24 @@
 'use strict';
 
-var _createClass = function () { 
-  function defineProperties(target, props) { 
-    for (var i = 0; i < props.length; i++) { 
-      var descriptor = props[i]; 
-      descriptor.enumerable = descriptor.enumerable || false; 
-      descriptor.configurable = true; 
-      if ("value" in descriptor) descriptor.writable = true; 
-      Object.defineProperty(target, descriptor.key, descriptor); 
-    } 
-  } return function (Constructor, protoProps, staticProps) { 
-    if (protoProps) defineProperties(Constructor.prototype, protoProps); 
-    if (staticProps) defineProperties(Constructor, staticProps); 
-    return Constructor; 
-  }; 
-}();
-
-function _classCallCheck(instance, Constructor) { 
-  if (!(instance instanceof Constructor)) { 
-    throw new TypeError("Cannot call a class as a function"); 
-  } 
+function Match(){
+  this.RegString = /^\d{1,}$/gi;
+  this.init();
 }
 
-var NumberValidator = function () {
-  function NumberValidator(submitButton, myForm) {
-    _classCallCheck(this, NumberValidator);
+Match.prototype.init = function(){
+  var _this = this;
+  var submitButton = document.getElementById('submit-button');
+  submitButton.addEventListener("click", function(){
+    _this.test();
+  }, false);
+};
 
-    this.submitButton = submitButton;
-    this.myForm = myForm;
-    this.init();
-  }
+Match.prototype.test = function(){
+  var dom = document;
+  var boxOne = dom.getElementById('box-one');
+  var boxTwo = dom.getElementById('box-two');
+  boxTwo.value = !isNaN(boxOne.value.trim());
+  
+};
 
-  _createClass(NumberValidator, [{
-    key: 'init',
-    value: function init() {
-      var numberPattern = /^[0-9]*(?:\.\d{1,2})?$/;
-      this.submitButton.addEventListener('click', function (formEvent) {
-        if (!this.validateField(formEvent, numberPattern)) {
-          formEvent.preventDefault();
-        }
-      }.bind(this));
-    }
-  }, {
-    key: 'validateField',
-    value: function validateField(formEvent, numberPattern) {
-      var numberTextField = document.getElementById('number');
-      var resultTextField = document.getElementById('result');
-      if (this.isEmpty(numberTextField.value)) {
-        numberTextField.focus();
-        resultTextField.value = 'Nothing to validate';
-        return false;
-      } else {
-        if (this.isNumber(numberPattern)) {
-          setTimeout(function () {
-            this.myForm.submit();
-          }, 1000); //seting Timeout to delay submission of form for 10 seconds to show result value
-          return false;
-        } else {
-          return false;
-        }
-        return true;
-      }
-    }
-  }, {
-    key: 'isNumber',
-    value: function isNumber(numberPattern) {
-      var numberTextField = document.getElementById('number');
-      var resultTextField = document.getElementById('result');
-      if (!numberPattern.test(numberTextField.value.trim())) {
-        resultTextField.value = 'False';
-        numberTextField.focus();
-        return false;
-      } else {
-        resultTextField.value = 'True';
-        return true;
-      }
-    }
-  }, {
-    key: 'isEmpty',
-    value: function isEmpty(textField) {
-      if (textField === null || textField.trim() === '') {
-        return true;
-      }
-      return false;
-    }
-  }]);
-
-  return NumberValidator;
-}();
-
-var submitButton = document.getElementById('submitButton');
-var myForm = document.getElementById('myForm');
-new NumberValidator(submitButton, myForm);
-
-
-
-
-
-
-/*const submitButton = document.getElementById('submitButton');
-const myForm = document.getElementById('myForm');
-new NumberValidator(submitButton, myForm);*/
-
-
-
-
-
-
-
+new Match();
--- exercise_twelve/Url_Checker.js
+++ exercise_twelve/Url_Checker.js
@@ -1,67 +1,38 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+function Match(){
+  this.urlRegEx = /^(https?:\/\/)?((www)\.)?((.+)\.)?((.+)\.([^\/]{2,}))(\/(.*))?$/i;// I removed the 'g' tag , some bad voodoo i need to understand
+  //this.subUrlRegEx = /^((\S+)?\.)?((\S+)\.(\S+))$/i;
+  this.init();
+}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+Match.prototype.init = function(){
+  var _this = this;
+  var submitButton = document.getElementById('submit-button');
+  submitButton.addEventListener("click", function(){
+    _this.test();
+  }, false);
+};
 
-var UrlChecker = function () {
-  function UrlChecker(submitButton) {
-    _classCallCheck(this, UrlChecker);
-
-    this.submitButton = submitButton;
-    this.init();
+Match.prototype.test = function() {
+  var testValue = document.getElementById('box-one').value;
+  if (testValue && testValue.trim().length > 0) {
+    console.log(testValue);
+    var validExp =  this.urlRegEx.exec(testValue.trim());
+    console.log(validExp);
+    var message;
+    if (validExp) {
+      message = 'Domain : ' + validExp[6];
+      if (validExp[5]) {
+        message += '\nSub Domain : ' + validExp[5];
+      }
+    } else {
+      message = 'Please provide us a valid Url';
+    }
+    alert(message);
+    return;
   }
+  alert('Please input a valid Url');
+};
 
-  _createClass(UrlChecker, [{
-    key: 'init',
-    value: function init() {
-      var urlTestPattern = /^(https?:\/\/)(www.)([^.\/.]+(?=\.)|)(\.?[^\/.]+\.[^\/]+)\/?(.+|)$/;
-      var urlExtractPattern = /^(?:https?:\/\/)?(?:www?.)?([^.\/.]+(?=\.)|)(\.?[^\/.]+\.[^\/]+)\/?(.+|)$/;
-      this.submitButton.addEventListener('click', function (formEvent) {
-        var urlInputField = document.getElementById('url');
-        if (!this.isEmpty(urlInputField) && this.isValid(urlInputField, urlTestPattern)) {
-          this.extractDomain(urlInputField, urlExtractPattern);
-        } else {
-          formEvent.preventDefault();
-        }
-      }.bind(this));
-    }
-  }, {
-    key: 'isEmpty',
-    value: function isEmpty(inputField) {
-      if (inputField.value === null || inputField.value.trim() === '') {
-        alert('URL Field cannot be empty');
-        inputField.focus();
-        return true;
-      }
-      return false;
-    }
-  }, {
-    key: 'isValid',
-    value: function isValid(inputField, urlTestPattern) {
-      if (!urlTestPattern.test(inputField.value)) {
-        alert('Invalid URL');
-        return false;
-      }
-      return true;
-    }
-  }, {
-    key: 'extractDomain',
-    value: function extractDomain(urlInputField, urlExtractPattern) {
-      var match = urlExtractPattern.exec(urlInputField.value);
-      if (match !== null) {
-        if (match[1] === null || match[1] === '') {
-          alert('Domain: ' + match[2]);
-        } else {
-          alert('Domain: ' + match[2].replace('.', ''));
-          alert('Subdomain: ' + match[1]);
-        }
-      }
-    }
-  }]);
-
-  return UrlChecker;
-}();
-
-var submitButton = document.getElementById('submitButton');
-new UrlChecker(submitButton);
+new Match();
